90b68ae27b04437385ee3b7e0fae172bb16e27ea
Radhep <Radhep.Saba@gmail.com>
2018-01-02 17:58:21 -0500

    App icon changed

--- app/src/main/java/radhep/funfacts/FunFactsActivity.java
+++ app/src/main/java/radhep/funfacts/FunFactsActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
