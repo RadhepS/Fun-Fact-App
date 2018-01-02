123a0226059511ffcc28197a43e016b5f5af88ee
Radhep <Radhep.Saba@gmail.com>
2018-01-02 13:27:23 -0500

    Functionaliy added to button press

--- app/src/main/java/radhep/funfacts/FunFactsActivity.java
+++ app/src/main/java/radhep/funfacts/FunFactsActivity.java
@@ -2,12 +2,33 @@ package radhep.funfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FunFactsActivity extends AppCompatActivity {
+
+    private TextView factTextView;
+    private Button showFactButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        //Assign the views from the layout file to the corresponding variables
+        factTextView = findViewById(R.id.factTextView);
+        showFactButton = findViewById(R.id.showFactButton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //The button is clicked therefore we update the fact TextView with a new fact
+                String fact = "Ostriches can run faster than horses";
+                factTextView.setText(fact);
+            }
+        };
+
+        showFactButton.setOnClickListener(listener);
     }
 }
